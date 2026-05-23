bf325779d2165802903555d2710f6fef43c1fb40
Daniel Francisco de Almeida <dan.franciscoo@gmail.com>
2026-05-23 19:26:19 -0300

    Initial commit

---
+++ src/test/java/br/com/catalogoFilmes/catalogoFilmes/CatalogoFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.catalogoFilmes.catalogoFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
