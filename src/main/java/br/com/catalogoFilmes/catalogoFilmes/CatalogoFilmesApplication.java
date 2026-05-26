70ee956bc72a28db023ec0b5331b1ed3a9d81238
Daniel Francisco de Almeida <dan.franciscoo@gmail.com>
2026-05-26 11:10:24 -0300

    Buscando por nome do episódios

--- src/main/java/br/com/catalogoFilmes/catalogoFilmes/CatalogoFilmesApplication.java
+++ src/main/java/br/com/catalogoFilmes/catalogoFilmes/CatalogoFilmesApplication.java
@@ -1,10 +1,6 @@
 package br.com.catalogoFilmes.catalogoFilmes;
 
-import br.com.catalogoFilmes.catalogoFilmes.client.ApiClient;
-import br.com.catalogoFilmes.catalogoFilmes.dto.EpisodioDTO;
-import br.com.catalogoFilmes.catalogoFilmes.dto.TemporadaDTO;
 import br.com.catalogoFilmes.catalogoFilmes.services.MediaSearchService;
-import br.com.catalogoFilmes.catalogoFilmes.util.JsonConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
